6123b6f138597081c2de39e76d354811841fec44
tbunch <timbunch@gmail.com>
2020-05-02 13:40:10 -0700

    build a contact form

---
+++ src/js/components/forms/ContactForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-form v-model="valid" @submit="submit">
+        <h2>Contact Us</h2>
+        <v-text-field
+                filled
+                type="text"
+                name="name"
+                label="Name"
+                v-model="name"
+                :rules="nameRules"
+                required
+        />
+        <v-text-field
+                filled
+                type="email"
+                name="email"
+                label="Email"
+                v-model="email"
+                :rules="emailRules"
+                required
+        />
+        <v-textarea
+                v-model="message"
+                name="message"
+                label="Message"
+                filled
+        />
+        <v-btn color="primary" :disabled="!valid" @click="submit">Submit</v-btn>
+    </v-form>
+</template>
+<script>
+    export default {
+      name: 'contact-form',
+      data() {
+        return {
+          name: null,
+          email: null,
+          message: null,
+          valid: false,
+          nameRules: [
+            v => !!v || 'Name is required'
+          ],
+          emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid',
+          ],
+        }
+      },
+      methods: {
+        submit() {
+          if (!this.valid) {
+            return false;
+          }
+        }
+      }
+    }
+</script>
--- src/js/pages/page.vue
+++ src/js/pages/page.vue
@@ -9,6 +9,7 @@
         </page-hero>
         <v-container>
             <post-content :content="page.content.rendered"/>
+            <contact-form v-if="$route.params.slug === 'contact-us'" />
             <tiles class="tiles-small" v-if="tiles" :tiles="tiles" :height="160"/>
         </v-container>
     </div>
@@ -19,10 +20,11 @@
   import PostContent from "../components/content/PostContent";
   import PageHero from "../components/page-elements/PageHero";
   import Tiles from "../components/tiles/Tiles";
+  import ContactForm from "../components/forms/ContactForm";
 
   export default {
     name: 'page',
-    components: {Tiles, PageHero, PostContent},
+    components: {ContactForm, Tiles, PageHero, PostContent},
     beforeRouteEnter(to, from, next) {
       store.dispatch('GET_PAGE', to.params.slug).then((page) => {
         store.dispatch('SET_PAGE_META', page);
